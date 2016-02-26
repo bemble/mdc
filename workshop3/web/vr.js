0ecb942998e6a006e5e77a0f08a656df7df19447
Pierre CLEMENT <pierre@clement.re>
2016-02-26 12:33:56 +0100

    double canvas

--- workshop3/web/vr.js
+++ workshop3/web/vr.js
@@ -1,29 +1,36 @@
-window.setSceneOrientation = function(x, y, z) {
-    document.getElementById('x').innerHTML = "x: " + x;
-    document.getElementById('y').innerHTML = "y: " + y;
-    document.getElementById('z').innerHTML = "z: " + z;
-};
-
 (function() {
-    var canvas = document.querySelector('canvas');
-    var ctx = canvas.getContext("2d");
+    window.onload = function() {
+        setTimeout(function() { window.scrollTo(0, 1) }, 100);
+    };
+    
+    var canvases = {}, ctxes = {};
+    ['left', 'right'].forEach(function(side) {
+       canvases[side] =  document.getElementById(side);
+       ctxes[side] =  canvases[side].getContext("2d");;
+    });
     
     
-    
-    ctx.fillStyle="#FF0000";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    
-    var grd=ctx.createLinearGradient(0,0,0,canvas.height);
-    grd.addColorStop(0, 'rgba(255, 255, 255, 0)');
-    grd.addColorStop(1,"rgba(255, 255, 255, 0.5)");
+    ['left', 'right'].forEach(function(side) {
+        var canvas = canvases[side];
+        var ctx = ctxes[side];
+        
+        ctx.fillStyle="#FF0000";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        
+        
+        var grd=ctx.createLinearGradient(0,0,0,canvas.height);
+        grd.addColorStop(0, 'rgba(255, 255, 255, 0)');
+        grd.addColorStop(1,"rgba(255, 255, 255, 0.5)");
 
-    ctx.fillStyle=grd;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle=grd;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    });
     
     window.addEventListener("deviceorientation", orientation, false);
     
     function orientation(event){
-        window.setSceneOrientation(event.beta, event.gamma, event.alpha);
+        document.getElementById('x').innerHTML = "x: " + event.beta;
+        document.getElementById('y').innerHTML = "y: " + event.gamma;
+        document.getElementById('z').innerHTML = "z: " + event.alpha;
     }
 })();
