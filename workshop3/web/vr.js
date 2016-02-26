230c2afdb6c58b6496b83c54c5ae57460d3bafbe
Pierre Fiorelli <Pierre Fiorelli>
2016-02-26 14:41:10 +0100

    add mountain

--- workshop3/web/vr.js
+++ workshop3/web/vr.js
@@ -27,16 +27,16 @@
 		var canvas = canvases.left;
 		var size = canvas.height/2;
 		
-		var colors = {r:102, g:187, b:106};
+		var colors = {r:56, g:142, b:60};
 		var pos = canvas.height/2;
 		for(var i=0; i<4; i++)
 		{
-			colors.r+=10;
-			colors.g+=10;
-			colors.b+=10;
-			var x = -width*0.5;
+			colors.r+=15;
+			colors.g+=15;
+			colors.b+=15;
+			var x = -size*0.5;
 			size -= canvas.height/10;
-			pos= canvas.height*1.0-(3-i)*size*0.1;
+			pos= canvas.height*1.0-(3-i)*size*0.2;
 			
 			var mountainsChain = new Mountains(pos, 'rgb('+colors.r+","+colors.g+","+colors.b+')', i+1)
 			mountainsChains.push(mountainsChain);
@@ -44,10 +44,10 @@
 			for(var j = 0; j<1000; j++)
 			{
 				var width = Math.random()*size*0.8+size;
-				x+=(width*0.5)*Math.random()+width*0.3;
 				
 				var mountain = new Mountain(x, width);
 				mountainsChain.list.push(mountain);
+				x+=(width*0.5)*Math.random()+width*0.3;
 			}
 		}
 	}
@@ -56,19 +56,19 @@
 	
 	// Clears the canvas and draws a mountain silohuette.  The mountain is a light
 	// purple.  Also renders the ruggedness value in black.
-	 function drawMountain(ctx, x, width, pos, color) {
+	 function drawMountain(ctx, x, width, pos, color, padding) {
 		ctx.beginPath();
-		ctx.moveTo(x, pos);
-		ctx.lineTo(x, pos-width/2);
-		ctx.lineTo(x+width/2, y = pos - width);
-		ctx.lineTo(x+width, pos-width/2);
-		ctx.lineTo(x+width, pos);
+		ctx.moveTo(x+padding, pos+padding);
+		ctx.lineTo(x+padding, pos-width/2+padding);
+		ctx.lineTo(x+width/2+padding, y = pos - width+padding);
+		ctx.lineTo(x+width+padding, pos-width/2+padding);
+		ctx.lineTo(x+width+padding, pos+padding);
 		ctx.closePath();
 		ctx.fillStyle = color;
 		ctx.fill();
 	};
 	
-	function drawMountains(ctx, position){
+	function drawMountains(ctx, position, padding){
 		var canvas = canvases.left;
 		ctx.fillStyle="#FF0000";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -91,32 +91,26 @@
 				var posX = mountain.x+position*mountains.speed;
 				
 				if(posX<canvas.width && posX + mountain.width>0)
-				drawMountain(ctx, posX, mountain.width, mountains.pos, mountains.color);
+				drawMountain(ctx, posX, mountain.width, mountains.pos, mountains.color, padding*i*2);
 			}
 		}
 	}
 
-	['left', 'right'].forEach(function(side) {
+	['left', 'right'].forEach(function(side, index) {
 		var canvas = canvases[side];
 		var ctx = ctxes[side];
 
 		var width = canvas.width, height = canvas.height;
 			
-			
-		
-		
-		
-		
-		
 		
 		function move(position){
-			drawMountains(ctx, position*50);
+			drawMountains(ctx, position*50, index);
 		}
 		
 		var position=0;
 		var interval = setInterval(function(){
 			position-=1;
-			drawMountains(ctx, position);
+			drawMountains(ctx, position, index);
 		}, 25);
 	});
 })();
